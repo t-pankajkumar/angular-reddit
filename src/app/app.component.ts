c6dd2ad25f785887711c5a8c861d57e414f5ddf5
pankaj kumar tumpala <pankaj kumar tumpala>
2017-04-12 10:19:44 +0530

    Form added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Article } from './article/article.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [];
+  }
+  addArticle(title: HTMLInputElement, link: HTMLInputElement, note: HTMLInputElement): boolean {
+    console.log(`Adding title:${title.value} and link:${link.value}`);
+    this.articles.push(new Article(title.value, link.value, note.value, 0));
+    title.value = '';
+    link.value = '';
+    note.value = '';
+    return false;
+  }
+  sortedArticle(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
